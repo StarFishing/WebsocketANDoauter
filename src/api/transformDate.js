e71f4934cb0e2ec88e865237bc7538271295cf2a
startfishing <2923627211@qq.com>
2019-06-11 20:08:38 +0800

    添加年份日期格式转化以及选择时间的位数补全

--- src/api/transformDate.js
+++ src/api/transformDate.js
@@ -1,8 +1,15 @@
-export function GMTToStr (data, time) {
+export function GMTToStr(data, time) {
   let date = new Date(data)
   // let arr = []
   // arr = time.split(':')
-  let arr = time
+  let arr = time.map((value, index) => {
+    if (index === 0) {
+      if (value.length === 1) return '00' + value
+      if (value.length === 2) return '0' + value
+    }
+    if (value.length === 1 && index !== 0) return '0' + value
+    else return value
+  })
   let day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate()
   let Month =
     date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)
@@ -19,4 +26,11 @@ export function GMTToStr (data, time) {
     ':' +
     date.getFullYear()
   return str
+}
+export function getLocalDate(data) {
+  let date = new Date(data)
+  let day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate()
+  let Month =
+    date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)
+  return date.getFullYear() + '-' + Month + '-' + day
 }
